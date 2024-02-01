cbe91a0c3026a23239915f28bb99aff2db29df9c
Cristian <crodg1023@outlook.com>
2024-02-01 20:09:35 +0100

    Cambio del router del login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,12 +11,15 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+
+  constructor(private router: Router) {}
   usuario: string = "";
   password: string = "";
 
   login() {
     console.log("login!");
-    window.location.href = "http://localhost:4200/empresas";
+    //window.location.href = "http://localhost:4200/empresas";
+    this.router.navigate(['/login']);
     // El redirect no es lo correcto
   }
 }
