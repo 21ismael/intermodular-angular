ff84af4ee0899cd3849d6a348364057a3d5c9093
Cristian <crodg1023@outlook.com>
2024-02-07 19:11:53 +0100

    Añadido del edit

---
+++ src/app/edit-tutor/edit-tutor.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-tutor',
+  standalone: true,
+  imports: [],
+  templateUrl: './edit-tutor.component.html',
+  styleUrl: './edit-tutor.component.scss'
+})
+export class EditTutorComponent {
+
+}
