dd7103d705b64b589d7e94e3b9d27d04dd7909fd
Ismael <ismaelauam@gmail.com>
2024-02-07 18:46:48 +0100

    Formulario nueva empresa

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,6 +8,7 @@ import { EmpresaDetailsComponent } from './empresas/empresas-dashboard/empresa-d
 import { AdminPanelComponent } from './admin-panel/admin-panel.component';
 import { AddCentroComponent } from './add-centro/add-centro.component';
 import { AddTutorComponent } from './add-tutor/add-tutor.component';
+import { AddEmpresaComponent } from './add-empresa/add-empresa.component';
 
 export const routes: Routes = [
   { path: '', component: LandingPageComponentComponent, title: 'Gestión FCT', data: { animation: 'landingPage' } },
@@ -16,7 +17,8 @@ export const routes: Routes = [
   { path: 'empresas/:id', component: EmpresaDetailsComponent, resolve: { empresa: detailsResolver } },
   { path: 'panel', component: AdminPanelComponent ,title: 'Panel de administración', children: [
     { path: 'add/centro', component: AddCentroComponent, title: 'Añadir Centro' },
-    { path: 'add/tutor', component: AddTutorComponent, title: 'Añadir Tutor' }
+    { path: 'add/tutor', component: AddTutorComponent, title: 'Añadir Tutor' },
+    { path: 'add/empresa', component: AddEmpresaComponent, title: 'Añadir Empresa' }
   ] },
   { path: '**', component: NotFoundComponent, title: 'Página no encontrada' }
 ];
