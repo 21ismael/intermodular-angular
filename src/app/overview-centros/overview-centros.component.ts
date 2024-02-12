6aaaedf4bfc5da6918ecb769e391a9de9cc9e327
Ismael <ismaelauam@gmail.com>
2024-02-12 18:30:07 +0100

    Servicio que devuelve los centros de la base de datos

--- src/app/overview-centros/overview-centros.component.ts
+++ src/app/overview-centros/overview-centros.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, RouterLink, RouterLinkActive } from '@angular/router';
 import {MatTooltipModule} from '@angular/material/tooltip';
 import { Usuario } from '../usuario';
+import { Centro } from '../centro';
 
 @Component({
   selector: 'app-overview-centros',
@@ -11,15 +12,14 @@ import { Usuario } from '../usuario';
   styleUrl: './overview-centros.component.scss'
 })
 export class OverviewCentrosComponent implements OnInit {
-  centros: Usuario[] = [];
+  centros: Centro[] = [];
 
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.route.data.subscribe(({ usuarios }) => {
-      const usrs : any = usuarios;
-      this.centros = usrs.data.filter((centro: Usuario) => centro.roles.includes('centro'));
-      console.log(this.centros);
+    this.route.data.subscribe(({ centros }) => {
+      this.centros = centros;
+      console.log(centros);
     });
   }
 }
